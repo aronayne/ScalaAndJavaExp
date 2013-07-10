8da3bc2a20f0aa16deb830f55817c43fb942f351
Adrian Ronayne <adrian.ronayne@gmail.com>
2013-07-10 18:47:43 +0100

    Update

---
+++ .worksheet/src/continuations.Continuations.scala
@@ -0,0 +1,27 @@
+package continuations
+
+object Continuations {;import org.scalaide.worksheet.runtime.library.WorksheetSupport._; def main(args: Array[String])=$execute{;$skip(107); 
+
+	def plus [A] (x: Int, y : Int, k: Int => A): A =
+		k(x + y);System.out.println("""plus: [A](x: Int, y: Int, k: Int => A)A""");$skip(61); 
+		
+	def times[A] (x: Int, y: Int, k: Int => A): A =
+		k(x*y);System.out.println("""times: [A](x: Int, y: Int, k: Int => A)A""");$skip(82); 
+		
+	def less [A] (x: Int, y: Int, kt: => A, kf: => A): A =
+		if(x < y) kt else kf;System.out.println("""less: [A](x: Int, y: Int, kt: => A, kf: => A)A""");$skip(150); 
+
+	def factorial [A] (n : Int, k : Int => A) : A =
+		less(n , 2 , k(1),
+			plus(n , -1 , (m: Int) =>
+				factorial(m, (f: Int) =>
+					times(n,f,k))));System.out.println("""factorial: [A](n: Int, k: Int => A)A""");$skip(17); 
+		println(3 + 4);$skip(23); 
+    plus(3,4, println);$skip(46); 
+    
+    less(3 , 2, println(3) , println(2));$skip(31); 
+    
+		factorial(5 , println);$skip(38); 
+		factorial(3, factorial(_, println));$skip(34); 
+    val n = factorial(3 , r => r);System.out.println("""n  : Int = """ + $show(n ))}
+}
